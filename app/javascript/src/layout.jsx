08fb19188b84d0bf7fa4ca66faedbd6dc123d91e
Jaymes Phimmachack <phimmachack.jaymes@gmail.com>
2019-07-12 12:35:50 +0000

    Uncomment authentication

--- app/javascript/src/layout.jsx
+++ app/javascript/src/layout.jsx
@@ -6,18 +6,18 @@ class Layout extends React.Component {
     authenticated: false,
   }
   componentDidMount() {
-   /* fetch('/api/authenticated')
+    fetch('/api/authenticated')
       .then(handleErrors)
       .then(data => {
         this.setState({
           authenticated: data.authenticated,
         })
-      })*/
+      })
   }
   renderLinks = () => {
-     //const { authenticated } = this.state;
+     const { authenticated } = this.state;
      
-   /* if(authenticated){
+    if(authenticated){
       return (
         <span>
           <li className="nav-item">
@@ -42,7 +42,7 @@ class Layout extends React.Component {
           </li>
         </span>
       );
-    }*/
+    }
   }
   render() {
    
